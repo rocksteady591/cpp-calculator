a877e319a35928c0ef9458192b65d70066ce2b0a
Pavel Galkin <rocksteady591@gmail.com>
2025-07-28 00:28:12 +0400

    added ready files

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -1,2 +1,107 @@
 // В этом файле определения функций.
 // За основу возьмите решение предыдущей задачи.
+#include "calculator.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <cmath>
+
+using Number = double;
+
+std::vector<char> OPERATIONS {'+', '-', '*', '/', '=', ':', 'c', 'q', 's', 'l'};
+std::pair<bool, Number> MEMORY {false, 0};
+
+bool ReadNumber(Number& result){
+    std::cin >> result;
+    if(std::cin.fail()){
+        std::cerr << "Error: Numeric operand expected";
+        return false;
+    }
+    return true;
+}
+
+bool IsCorrectOperation(std::string& operation){
+    if(operation.size() == 2 && operation == "**"){
+        return true;
+    }else if(operation.size() == 1){
+        for(int i = 0; i < OPERATIONS.size(); i++){
+            if(operation[0] == OPERATIONS[i]){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool ReadOperation(std::string& operation){
+    std::cin >> operation;
+    if(!IsCorrectOperation(operation)){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+bool RunCalculatorCycle(){
+    Number number = 0;//самое первое число которое подается на ввод
+    std::string operation;//эта переменная используется и для ввода команд и для ввода
+    if(!ReadNumber(number)){
+        return false;
+    }
+    while(true){
+        if(!ReadOperation(operation)){
+            std::cerr << "Error: Unknown token " << operation;
+            return false;
+        }else{
+            Number num = 0;
+            if(operation == "+"){
+                if(!ReadNumber(num)){
+                    return false;
+                }
+                number += num;
+            }else if(operation == "-"){
+                if(!ReadNumber(num)){
+                    return false;
+                }
+                number -= num;
+            }else if(operation == "*"){
+                if(!ReadNumber(num)){
+                    return false;
+                }
+                number *= num;
+            }else if(operation == "**"){
+                if(!ReadNumber(num)){
+                    return false;
+                }
+                number = std::pow(number, num);
+            }else if(operation == "/"){
+                if(!ReadNumber(num)){
+                    return false;
+                }
+                number /= num;
+            }else if(operation == "="){
+                std::cout << number << std::endl;
+            }else if(operation == ":"){
+                if(!ReadNumber(num)){
+                    return false;
+                }
+                number = num;
+            }else if(operation == "c"){
+                number = 0;
+            }else if(operation == "q"){
+                return true;
+            }else if(operation == "s"){
+                MEMORY = {true, number};
+            }else if(operation == "l"){
+                if(MEMORY.first == false){
+                    std::cerr << "Error: Memory is empty" << std::endl;
+                    return false;
+                }else{
+                    number = MEMORY.second;
+                }
+            }
+        }
+    }
+}
--- calculator/calculator.h
+++ calculator/calculator.h
@@ -1,1 +1,15 @@
 // В этом файле объявления функций.
+#pragma once
+#include <string>
+using Number = double;
+// Возвращает true, если удалось прочитать число
+// и сохранить его в Number.
+bool ReadNumber(Number& result);
+//Проверяет корректность введенной операции
+//возвращает true если такая операция есть и false если нет
+bool IsCorrectOperation(std::string& operation);
+//считывает операцию с консоли и передает ее в IsCorrectOperation
+bool ReadOperation(std::string& operation);
+// Возвращает true, если работа завершилась штатно 
+// командой q. Если из-за ошибки, возвращает false.
+bool RunCalculatorCycle(); 
